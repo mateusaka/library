f7a1ecc800f9afc06bd86ff41f17779397c9160c
Mateus <correia.mateus@hotmail.com>
2023-10-30 11:28:20 -0300

    Setup ejs

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
 app.listen(PORT, () => {
     console.log("Server is up!");
