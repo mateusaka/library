bb6e12940de7b2c8a2d683f51704b175ba4983a0
Mateus <correia.mateus@hotmail.com>
2023-10-30 11:46:01 -0300

    Setup db

---
+++ database/database.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const db = mongoose.connect(process.env.DB_URL);
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
+const db = require("./database/database");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -13,4 +17,11 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
     console.log("Server is up!");
+
+    db.then(() => {
+        console.log("DB is up!");
+    })
+    .catch(error => {
+        console.log("Error on DB: " + error);
+    });    
 });
