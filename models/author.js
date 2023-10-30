2ad808b89c45be6e0831b00e8f2184c99ac9792e
Mateus <correia.mateus@hotmail.com>
2023-10-30 16:20:57 -0300

    Models

---
+++ models/author.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        maxLength: 100
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxLength: 100
+    },
+    dateOfBirth: {
+        type: Date
+    },
+    dateOfDeath: {
+        type: Date
+    }
+});
+
+AuthorSchema.virtual("name").get(function () {
+    let fullName = "";
+
+    if(this.firstName && this.lastName) {
+        fullName = `${this.lastName}, ${this.firstName}`
+    }
+
+    return fullName;
+});
+
+AuthorSchema.virtual("url").get(function () {
+    return `/catalog/author/${this._id}`;
+});
+
+module.exports = mongoose.model("Author", AuthorSchema);
